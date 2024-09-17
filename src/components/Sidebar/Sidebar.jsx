a15407854470a6624996db1890ce6b8999efac89
swe-robertkibet <swe.robertkibet@gmail.com>
2024-09-17 18:27:17 +0300

    Update sidebar styles - new chat section

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -6,7 +6,7 @@ const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="top">
-        <img src={assets.menu_icon} alt="" />
+        <img className="menu" src={assets.menu_icon} alt="" />
         <div className="new-chat">
           <img src={assets.plus_icon} alt="" />
           <p>New Chat</p>
