85525853c37c38591d3dc163f682c2ee0f816364
swe-robertkibet <swe.robertkibet@gmail.com>
2024-09-20 23:28:05 +0300

    Update context.jsx - added context values, to use on the side bar and main components

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -5,7 +5,7 @@ export const Context = createContext();
 
 const ContextProvider = (props) => {
   const [input, setInput] = useState("");
-  const [recentPrompt, setPrompt] = useState("");
+  const [recentPrompt, setRecentPrompt] = useState("");
   const [prevPrompts, setPrevPrompts] = useState([]);
   const [showResult, setShowResult] = useState(false);
   const [loading, setLoading] = useState(false);
@@ -15,7 +15,18 @@ const ContextProvider = (props) => {
     await run(prompt);
   };
 
-  const contextValue = {};
+  const contextValue = {
+    prevPrompts,
+    setPrevPrompts,
+    onSent,
+    setRecentPrompt,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    input,
+    setInput,
+  };
   return (
     <Context.Provider value={contextValue}>{props.children}</Context.Provider>
   );
