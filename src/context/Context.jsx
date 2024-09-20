2b226df4e00a35389459e5a8e0eff5a413e769af
swe-robertkibet <swe.robertkibet@gmail.com>
2024-09-20 23:21:10 +0300

    Update context.jsx - added several useStates to display data

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,14 +1,19 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import run from "../config/gemini";
 
 export const Context = createContext();
 
 const ContextProvider = (props) => {
+  const [input, setInput] = useState("");
+  const [recentPrompt, setPrompt] = useState("");
+  const [prevPrompts, setPrevPrompts] = useState([]);
+  const [showResult, setShowResult] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [resultData, setResultData] = useState("");
+
   const onSent = async (prompt) => {
     await run(prompt);
   };
-
-  onSent("What is react js");
 
   const contextValue = {};
   return (
