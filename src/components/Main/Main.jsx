0f474d2838b066d9d0b7102259466177895a0302
swe-robertkibet <swe.robertkibet@gmail.com>
2024-09-23 14:10:22 +0300

    Update Main.jsx - hide greet and cards onSent

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -22,32 +22,37 @@ const Main = () => {
       </div>
 
       <div className="main-container">
-        <div className="greet">
-          <p>
-            <span>Hello, Dev.</span>
-          </p>
+        {!showResult ? (
+          <>
+            <div className="greet">
+              <p>
+                <span>Hello, Dev.</span>
+              </p>
 
-          <p>How can I help you today?</p>
-        </div>
-
-        <div className="cards">
-          <div className="card">
-            <p>Improve readability of the following code</p>
-            <img src={assets.code_icon} alt="" />
-          </div>
-          <div className="card">
-            <p>Briefly summarize this concept: urban planning</p>
-            <img src={assets.bulb_icon} alt="" />
-          </div>
-          <div className="card">
-            <p>Brainstorm team bonding activities for our work retreat</p>
-            <img src={assets.message_icon} alt="" />
-          </div>
-          <div className="card">
-            <p>Suggest beautiful places to see on an upcoming road trip</p>
-            <img src={assets.compass_icon} alt="" />
-          </div>
-        </div>
+              <p>How can I help you today?</p>
+            </div>
+            <div className="cards">
+              <div className="card">
+                <p>Improve readability of the following code</p>
+                <img src={assets.code_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>Briefly summarize this concept: urban planning</p>
+                <img src={assets.bulb_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>Brainstorm team bonding activities for our work retreat</p>
+                <img src={assets.message_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>Suggest beautiful places to see on an upcoming road trip</p>
+                <img src={assets.compass_icon} alt="" />
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="result"></div>
+        )}
 
         <div className="main-bottom">
           <div className="search-box">
