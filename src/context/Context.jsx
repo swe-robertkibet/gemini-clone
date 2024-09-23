e2d049dd836d3f4a894e565eaaf54609604eb85a
swe-robertkibet <swe.robertkibet@gmail.com>
2024-09-23 14:54:02 +0300

    Update Main.jsx - added result text to sue bold for double **

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -11,13 +11,24 @@ const ContextProvider = (props) => {
   const [loading, setLoading] = useState(false);
   const [resultData, setResultData] = useState("");
 
+  const delayPara = (index, nextWord) => {};
+
   const onSent = async (prompt) => {
     setResultData("");
     setLoading(true);
     setShowResult(true);
     setRecentPrompt(input);
     const response = await run(input);
-    setResultData(response);
+    let responseArray = response.split("**");
+    let newResponse;
+    for (let i = 0; i < responseArray.length; i++) {
+      if (i === 0 || i % 2 !== 0) {
+        newResponse += responseArray[i];
+      } else {
+        newResponse += "<b>" + responseArray[i] + "</b>";
+      }
+    }
+    setResultData(newResponse);
     setLoading(false);
     setInput("");
   };
